80c459bd3372374fe8e73a2ffd5e967cfd5bd191
manjari-maduri <95037999+manjari-maduri@users.noreply.github.com>
2021-11-25 22:12:22 +0530

    Add files via upload

---
+++ aws-live-master/config.py
@@ -0,0 +1,6 @@
+customhost = "rtprojectdb.cqxubbdkaaha.ap-south-1.rds.amazonaws.com"
+customuser = "manjari8"
+custompass = "manjari8"
+customdb = "employee"
+custombucket = "rtproject"
+customregion = "ap-south-1"
